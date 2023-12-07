e946922e11bb71bb2bee0d82a147a254a6f1eca0
ksdyb.park <ksdyb@amazon.com>
2023-12-07 14:43:32 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -50,7 +50,6 @@ useMultipleUpload = os.environ.get('useMultipleUpload', 'false')
 useMultipleRAG = True
 kendraIndex = os.environ.get('kendraIndex')
 roleArn = os.environ.get('roleArn')
-maxOutputTokens = int(os.environ.get('maxOutputTokens'))
 numberOfRelevantDocs = os.environ.get('numberOfRelevantDocs', '10')
 top_k = int(numberOfRelevantDocs)
 selected_LLM = 0
@@ -64,7 +63,7 @@ print('connection_url: ', connection_url)
 
 HUMAN_PROMPT = "\n\nHuman:"
 AI_PROMPT = "\n\nAssistant:"
-def get_parameter(modelId, model_type):
+def get_parameter(modelId, model_type, maxOutputTokens):
     if modelId == 'amazon.titan-tg1-large' or modelId == 'amazon.titan-tg1-xlarge' or model_type=='titan': 
         return {
             "maxTokenCount":1024,
@@ -1651,7 +1650,8 @@ def getResponse(connectionId, jsonBody):
             }            
         )
     )
-    parameters = get_parameter(modelId, profile['model_type'])
+
+    parameters = get_parameter(modelId, profile['model_type'], profile['maxOutputTokens'])
 
     # langchain for bedrock
     llm = Bedrock(
